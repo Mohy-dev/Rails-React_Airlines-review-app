11ad124612f5c11a30d3aeba0296f4f51d69c0de
mohy-dev <mohy.dev@gmail.com>
2022-08-06 02:29:57 +0200

    seeder

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,7 +1,63 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+
+airlines = Airline.create([
+  {
+    name: "United Airlines",
+    image_url: "https://open-flights.s3.amazonaws.com/United-Airlines.png"
+  },
+  {
+    name: "Southwest",
+    image_url: "https://open-flights.s3.amazonaws.com/Southwest-Airlines.png"
+  },
+  {
+    name: "Delta",
+    image_url: "https://open-flights.s3.amazonaws.com/Delta.png"
+  },
+  {
+    name: "Alaska Airlines",
+    image_url: "https://open-flights.s3.amazonaws.com/Alaska-Airlines.png"
+  },
+  {
+    name: "JetBlue",
+    image_url: "https://open-flights.s3.amazonaws.com/JetBlue.png"
+  },
+  {
+    name: "American Airlines",
+    image_url: "https://open-flights.s3.amazonaws.com/American-Airlines.png"
+  }
+])
+
+  Review.create([
+  {
+    airline: airlines.first,
+    title: "Good Flight",
+    score: 5,
+    description: "I had a great time with this airline. The staff were very helpful and the plane was very clean and well maintained. I would recommend this airline to anyone who is looking for a great experience."
+  },
+  {
+    airline: airlines.first,
+    title: "Good Flight",
+    score: 4,
+    description: "I had a great time with this airline. The staff were very helpful and the plane was very clean and well maintained. I would recommend this airline to anyone who is looking for a great experience."
+  },
+  {
+    airline: airlines.first,
+    title: "Bad Flight",
+    score: 3,
+    description: "Nice"
+  },
+  {
+    airline: airlines.first,
+    title: "Bad Flight",
+    score: 2,
+    description: "Not good, over all"
+  },
+  {
+    airline: airlines.first,
+    title: "Bad Flight",
+    score: 1,
+    description: "I had a bad time."
+  }
+])
